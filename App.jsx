e31323c45268e5c955b50488cf24d44ca283b5d0
Dinkus <robertsnaer901@gmail.com>
2020-12-08 23:47:44 +0000

    stuff

--- App.jsx
+++ App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import StackNavigator from './src/routes';
 import reducers from './src/reducers';
 
 export default function App() {
   return (
-    <Provider store={createStore(reducers)}>
+    <Provider store={createStore(reducers, applyMiddleware(thunk))}>
       <StackNavigator />
     </Provider>
   );
