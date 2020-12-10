48d45b68864f16cd35a54c7d28ad39f341b6f63c
Daníel Þór Gestsson <danni539953@gmail.com>
2020-12-10 23:39:14 +0000

    blabla

--- src/components/Upcoming/index.jsx
+++ src/components/Upcoming/index.jsx
@@ -3,36 +3,6 @@ import { View, Text, TouchableOpacity, Image } from 'react-native';
 import styles from './styles';
 import ReactPlayer from 'react-player'
 
-<<<<<<< HEAD
-const Upcoming = ({
-  thumbnail,
-  name,
-  releaseDate,
-  trailerUri,
-}) => (
-  <TouchableOpacity
-    activeOpacity={0.8}
-  >
-    <View>
-      <Text>Make stuff happen beggi!</Text>
-      <Image style={styles.thumbnail}
-        resizeMode="cover"
-        source={{ uri: thumbnail}}
-      />
-      <Text>{name}</Text>
-      <Text>{releaseDate}</Text>
-      <ReactPlayer
-        url= {trailerUri}
-        width= "100%"
-        height= "40%"
-        controls= {true}
-      />
-    </View>
-  </TouchableOpacity>
-);
-=======
-
-
 class Upcoming extends React.Component {
   render() {
     return (
@@ -50,7 +20,6 @@ class Upcoming extends React.Component {
     );
   }
 }
->>>>>>> 043d3347fd6a6e2c994d43ada87cb4590f28eae7
 
 export default Upcoming;
 
