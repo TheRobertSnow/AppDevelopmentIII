6e5146e5d2b7ed0a571053c3e340110a910ef2d6
Dinkus <robertsnaer901@gmail.com>
2020-12-10 18:29:33 +0000

    added Menu, enabled navigation to Cinemas view from Menu view

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,8 +4,10 @@ import { createStackNavigator } from 'react-navigation-stack';
 import Cinemas from '../views/Cinemas';
 import CinemaDetail from '../views/CinemaDetail';
 import Movies from '../views/Movies';
+import Menu from '../views/Menu';
 
 const StackNavigator = createStackNavigator({
+  Menu,
   Cinemas,
   CinemaDetail,
   Movies,
