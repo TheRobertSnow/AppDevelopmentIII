7f1de92ff6c5f8dd6cb2e2a84b9275b760136123
Daníel Þór Gestsson <danni539953@gmail.com>
2020-12-10 21:54:16 +0000

    laga ehv sma error

--- src/components/CinemaDetails/index.jsx
+++ src/components/CinemaDetails/index.jsx
@@ -5,22 +5,16 @@ import styles from './styles';
 
 class CinemaDetails extends React.Component {
   render(){
-    setTimeout(console.log(this.props.cinemas.filter(cinema => cinema.id == this.props.currentCinema)),3000)
     return(
       <View>
         <FlatList
           scrollEnabled={false}
           numColumns={1}
-         
           data={this.props.cinemas.filter(cinema => cinema.id == this.props.currentCinema)}
-          
-          //{...setTimeout(console.log(data),3000)}
           renderItem={ ({ item: { id, name, description, address, city, phone, website } }) => {
-            
             return (
               <TouchableOpacity activeOpacity={0.8}>
                 <View style={styles.container}>
-                  
                   <Text style={styles.containerName}>{name}</Text>
                   <Text style={styles.containerText}>{description}</Text>
                   <Text style={styles.containerText}>{address}</Text>
