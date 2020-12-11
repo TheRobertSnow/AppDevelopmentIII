0cec956b5919d4405c351fdce80109c8e582744e
Dinkus <robertsnaer901@gmail.com>
2020-12-11 14:09:58 +0000

    made a fancy main menu

--- src/views/Menu/index.jsx
+++ src/views/Menu/index.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, Image } from 'react-native';
+import styles from './styles';
+import icon from '../../resources/12759.jpg';
 
 class Menu extends React.Component {
   render() {
     return (
-      <View>
-        <TouchableOpacity onPress={() => this.props.navigation.navigate('Cinemas')}>
-          <Text>Cinemas</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => this.props.navigation.navigate('Upcoming')}>
-          <Text>Upcoming Movies</Text>
-        </TouchableOpacity>
+      <View style={styles.container}>
+        <View style={styles.imageContainer}>
+          <Image style={styles.image} source={icon} />
+          <Text style={styles.title}>Dr. Cinema</Text>
+        </View>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={() => this.props.navigation.navigate('Cinemas')}>
+            <Text style={styles.buttonText}>Cinemas</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => this.props.navigation.navigate('Upcoming')}>
+            <Text style={styles.buttonText}>Upcoming Movies</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
---
+++ src/views/Menu/styles.js
@@ -0,0 +1,51 @@
+import { StyleSheet } from 'react-native';
+import { Dimensions } from 'react-native';
+
+const window = Dimensions.get('window');
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  imageContainer: {
+    width: window.width,
+    height: window.height/2,
+    alignItems: 'center',
+  },
+  image: {
+    flex: 1,
+    resizeMode: 'contain',
+  },
+  title: {
+    fontFamily: 'Roboto',
+    fontSize: 40,
+  },
+  buttonContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    width: window.width-150,
+    height: 100,
+    backgroundColor: '#fbfbfb',
+    borderRadius: 15,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 25,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.22,
+    shadowRadius: 2.22,
+    elevation: 3,
+  },
+  buttonText: {
+    fontSize: 20,
+  },
+});
+// #fbfbfb
